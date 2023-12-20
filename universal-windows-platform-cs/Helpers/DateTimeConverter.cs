939f81062a9d5d8df7fc72c9b23e7dfc78d66369
Truong Tam <tamtm@icd-vn.com>
2023-12-20 13:47:31 +0700

    Add DateTimeConverter

---
+++ universal-windows-platform-cs/Helpers/DateTimeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace universal_windows_platform_cs.Helpers
+{
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var d = value as DateTime?;
+            if (!d.HasValue) return value;
+            return d.Value.ToString("yyyy/MM/dd");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
