988cc38836e1425eb45ce92a36f64dee13b055f5
Truong Tam <tamtm@icd-vn.com>
2023-12-27 16:27:26 +0700

    Add remove company

--- universal-windows-platform-cs.Core/Services/CompanyService.cs
+++ universal-windows-platform-cs.Core/Services/CompanyService.cs
@@ -83,5 +83,41 @@ namespace universal_windows_platform_cs.Core.Services
                 }
             }
         }
+
+        public static async Task<bool> Remove(int CompanyId)
+        {
+            await Task.CompletedTask;
+            using (var db = new UWPContext())
+            {
+                try
+                {
+                    Company _Company = db.Company.Where(company => company.CompanyId == CompanyId).First();
+                    List<Order> _Order = db.Order.Where(order => order.CompanyId == _Company.CompanyId).ToList();
+
+                    foreach (Order _OrderItem in _Order)
+                    {
+                        List<Product> _Product = db.Product.Where(product => product.OrderId == _OrderItem.OrderId).ToList();
+                        if (_Product != null)
+                        {
+                            foreach (Product _ProductItem in _Product)
+                            {
+                                db.Product.Remove(_ProductItem);
+                            }
+                        }
+                        db.Order.Remove(_OrderItem);
+                    }
+
+                    db.Company.Remove(_Company);
+                    db.SaveChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                    return false;
+                }
+            }
+        }
+
     }
 }
--- universal-windows-platform-cs/ViewModels/CompanyDetailViewModel.cs
+++ universal-windows-platform-cs/ViewModels/CompanyDetailViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
-
+using Microsoft.Toolkit.Mvvm.Input;
 using universal_windows_platform_cs.Core.Models;
 using universal_windows_platform_cs.Core.Services;
+using universal_windows_platform_cs.Services;
+using universal_windows_platform_cs.Views;
 
 namespace universal_windows_platform_cs.ViewModels
 {
@@ -16,9 +18,11 @@ namespace universal_windows_platform_cs.ViewModels
     public class CompanyDetailViewModel : ObservableObject
     {
         public CompanyDetailView Item { get; set; }
+        public ICommand RemoveCompnayCommand { get; set; }
 
         public CompanyDetailViewModel()
         {
+            RemoveCompnayCommand = new RelayCommand<int>(RemoveCompany);
         }
 
         public async Task InitializeAsync(int CompanyId)
@@ -39,6 +43,12 @@ namespace universal_windows_platform_cs.ViewModels
                 Phone = data.Phone,
                 FullAddress = $"{data.ContactName} {data.Address}, {data.City}, {data.Country}"
             };
+        }
+
+        private async void RemoveCompany(int CompnayId)
+        {
+            await CompanyService.Remove(CompnayId);
+            NavigationService.Navigate<CompanyListPage>();
         }
     }
 }
