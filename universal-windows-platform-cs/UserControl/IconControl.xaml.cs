f43742a669a95cdfad28a953ecd2341f7291aec9
Truong Tam <tamtm@icd-vn.com>
2024-01-24 10:44:08 +0700

    Use UserControl

---
+++ universal-windows-platform-cs/UserControl/IconControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.UI.Xaml.Media;
+
+namespace universal_windows_platform_cs.UserControl
+{
+    public partial class IconControl : Windows.UI.Xaml.Controls.UserControl
+    {
+        private string ImageSource { get; set; }
+        private readonly string NoImage = "/Assets/icons8-multiply-48.png";
+
+        public IconControl()
+        {
+            InitializeComponent();
+        }
+
+        public string Icon
+        {
+            get
+            {
+                if (ImageSource == null)
+                {
+                    return NoImage;
+                }
+                return ImageSource;
+            }
+            set
+            {
+                switch (value)
+                {
+                    case "Setting":
+                        ImageSource = "/Assets/StoreLogo.png";
+                        break;
+                    default:
+                        ImageSource = NoImage;
+                        break;
+                }
+            }
+        }
+    }
+}
--- universal-windows-platform-cs/ViewModels/MainViewModel.cs
+++ universal-windows-platform-cs/ViewModels/MainViewModel.cs
@@ -1,19 +1,29 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using universal_windows_platform_cs.Core.Services;
 
 namespace universal_windows_platform_cs.ViewModels
 {
     public class MainViewModel : ObservableObject
     {
+        public ICommand MainViewCommand { get; set; }
         public MainViewModel()
         {
+            MainViewCommand = new RelayCommand(DebugCommand);
         }
 
         public async Task LoadDataAsync()
         {
             await DataSourceService.TestConnection();
+        }
+
+        private void DebugCommand()
+        {
+            Debug.WriteLine($"DebugCommand");
         }
     }
 }
